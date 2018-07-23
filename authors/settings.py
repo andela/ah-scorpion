5bafd4e86a332fded83d5f76934f1535878cdb1f
lenny kioko <lenny@lennys-MacBook-Pro.local>
2018-07-23 21:38:12 +0300

    chore (database): configure PostgreSQL as DB Engine
    - modify database settings
    - add pyscopg2
    
    [Delivers #159053979]

--- authors/settings.py
+++ authors/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'authors.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ah-scorpion',
+        'USER': 'team-scorpion',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
