5f3093a77894ebafd2efcabd228b2a2ba0a1f20e
lenny kioko <lenny@lennys-MacBook-Pro.local>
2018-07-24 19:36:23 +0300

    refactor (environment variables): change setup of environment variables
    use os.environ which raises an error if variable is missing
    
    [Finishes #159053979]

--- authors/settings.py
+++ authors/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'authors.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv("NAME"),
-        'USER': os.getenv("USER"),
-        'PASSWORD': os.getenv("PASSWORD"),
-        'HOST': os.getenv("HOST"),
-        'PORT': os.getenv("PORT"),
+        'NAME': os.environ["NAME"],
+        'USER': os.environ["USER"],
+        'PASSWORD': os.environ["PASSWORD"],
+        'HOST': os.environ["HOST"],
+        'PORT': os.environ["PORT"],
     }
 }
 
