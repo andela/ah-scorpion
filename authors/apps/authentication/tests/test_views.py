1d4046d312ea4b67b36992450614de42586f7311
Ruth Njeri Waiganjo <waiganjo@Ruths-MacBook-Pro-2.local>
2018-07-30 15:58:48 +0300

    [ft #159053972]test user registration

--- authors/apps/authentication/tests/test_views.py
+++ authors/apps/authentication/tests/test_views.py
@@ -16,14 +16,53 @@ class AuthenticationTests(APITestCase):
                 "password": "jakejake23"
             }
         }
+        # Set up the registration url.
+        self.reg_url = reverse('authentication:reg')
 
     def test_register_user(self):
         """
         Test that a user can register.
         """
-        url = reverse('authentication:reg')
-        response = self.client.post(url, self.data, format='json')
+        response = self.client.post(self.reg_url, self.data, format='json')
         self.assertEqual(response.data['email'], self.data['user']['email'])
         self.assertEqual(response.data['username'],
                          self.data['user']['username'])
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_register_without_user_name(self):
+        """
+        Test that a user cannot register without a username.
+        """
+        self.data['user']['username'] = ""
+        response = self.client.post(self.reg_url, self.data, format='json')
+        self.assertEqual(
+            response.data['errors']['username'][0], "This field may not be blank.")
+
+    def test_register_empty_user_name(self):
+        """
+        Test that a user cannot register empty string.
+        """
+        self.data['user']['username'] = " "
+        response = self.client.post(self.reg_url, self.data, format='json')
+        self.assertEqual(
+            response.data['errors']['username'][0], "This field may not be blank.")
+
+    def test_weak_password(self):
+        """
+        Test that a user enters a password containing 8 characters
+        with numbers and letters.
+        """
+        self.data['user']['password'] = "1234"
+        response = self.client.post(self.reg_url, self.data, format='json')
+        print(response.data['errors']['password'][0])
+        self.assertEqual(response.data['errors']['password'][0],
+                         "Password invalid, Password must be 8 characters long, include numbers and letters and have no spaces")
+
+    def test_email_wrong_format(self):
+        """
+        Test that the user enters an email in the correct format
+        """
+        self.data['user']['email'] = "jgmail"
+        response = self.client.post(self.reg_url, self.data, format='json')
+        self.assertEqual(response.data['errors']['email']
+                         [0], "Enter a valid email address.")
