f7908869c120d878baeda1f52e40ec83f8eaeedf
lenny kioko <lenny@lennys-MacBook-Pro.local>
2018-08-06 21:44:56 +0300

    [feature  #159053984] Add user's profile picture from provider

--- authors/apps/authentication/views.py
+++ authors/apps/authentication/views.py
@@ -1,6 +1,7 @@
 import datetime
 import jwt
 import json
+import requests
 from django.contrib.sites.shortcuts import get_current_site
 from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
@@ -193,14 +194,20 @@ class ResetPasswordDoneAPIView(APIView):
 
 class SocialAuth(APIView):
     """
-    Method that allows for social signup and login using Google and Facebook
+    Allows for social signup and login using Google and Facebook
     """
     permission_classes = (AllowAny,)
     serializer_class = SocialAuthSerializer
     renderer_classes = (UserJSONRenderer,)
 
     def post(self, request):
-
+        """
+        Receives the access_token and provider from the request,
+        once authentication is comlpete, it creates a new user record
+        if it does exist already. The user's information (username, email and image)
+        are saved and the user is provided with a JWT token for authorization when
+        using our API.
+        """
         # Get the access_token and provider from request
         # the access_token is provided by the particular provider
         # which in our case is either 'google-oauth2' or 'facebook'
@@ -208,6 +215,7 @@ class SocialAuth(APIView):
         serializer.is_valid(raise_exception=True)
 
         provider = serializer.data.get('provider')
+        access_token = serializer.data.get('access_token')
 
         # strategy sets up the required custom configuration for working with Django
         strategy = load_strategy(request)
@@ -217,36 +225,61 @@ class SocialAuth(APIView):
             # Loads backends defined on SOCIAL_AUTH_AUTHENTICATION_BACKENDS,
             # checks the appropiate one by using the provider given
             backend = load_backend(strategy=strategy, name=provider, redirect_uri=None)
-        except MissingBackend as e:
+
+        except MissingBackend:
             return Response({
                 "errors": {
                     "provider": ["Invalid provider"]
                 }
             }, status=status.HTTP_400_BAD_REQUEST)
  
-        access_token = serializer.data.get('access_token')
-
         try:
             # authenticates the user and 
             # creates a user in our user model if a user with
             # the given email and username does not exist already.
             # If the user exists, we just authenticate the user.
             user =  backend.do_auth(access_token)
-        except HTTPError as e:
-            return Response({
-                "error" : str(e),
-            }, status=status.HTTP_400_BAD_REQUEST)
 
+        except BaseException as error:
+            return Response({
+                "error" : str(error),
+            }, status=status.HTTP_400_BAD_REQUEST)
+        
         # Since the user is using social authentication, there is no need for email verification.
         # We therefore set the user to active here.
         if not user.is_active:
             user.is_active = True
             user.save()
 
+        def get_image_url(self):
+            """
+            gets the user's current image url from the provider.
+            saves/updates the image field of the particular user
+            and returns the image_url
+            """
+            try:
+                if provider == "google-oauth2":
+                    url = "http://picasaweb.google.com/data/entry/api/user/{}?alt=json".format(user.email)
+                    data = requests.get(url).json()
+                    image_url = data["entry"]["gphoto$thumbnail"]["$t"]
+
+                elif provider == "facebook":
+                    id_url = "https://graph.facebook.com/me?access_token={}".format(access_token)
+                    id_data = requests.get(id_url).json()
+                    user_id = id_data["id"]
+                    url = "http://graph.facebook.com/{}/picture?type=small".format(user_id)
+                    image_url = requests.get(url, allow_redirects=True).url
+
+            except BaseException:
+                image_url = ""
+
+            user.image = image_url
+            return image_url
+        
         serializer = UserSerializer(user)
         token = generate_token(serializer.data)
         output = serializer.data
         output["token"] = token
         output['bio'] = serializer.instance.bio
-        output['image'] = serializer.instance.image
+        output['image'] = get_image_url(self)
         return Response(output, status=status.HTTP_200_OK)
