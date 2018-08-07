7a79216acaedb47930be4dc1a010f52d1bc293c3
davenjeru <dave.njeru@andela.com>
2018-08-07 10:50:08 +0300

    [chore #159459856] Refactor code to meet README instructions  - rebase to develop  - change 'url' to 'path' to avoid regex
    Signed-off-by: davenjeru <dave.njeru@andela.com>

--- authors/apps/authentication/urls.py
+++ authors/apps/authentication/urls.py
@@ -1,5 +1,5 @@
-from django.conf.urls import url
 from django.urls import path
+
 from . import account_activator
 from .views import (LoginAPIView, RegistrationAPIView,
                     UserRetrieveUpdateAPIView, ResetPasswordAPIView,
@@ -13,12 +13,10 @@ urlpatterns = [
     path('users/login/', LoginAPIView.as_view(), name='login'),
     path('user/', UserRetrieveUpdateAPIView.as_view(), name='current_user'),
     path('activate/<str:token>', account_activator.activate, name='activate'),
-    url(r'^reset-password/', ResetPasswordAPIView.as_view(),
-        name='reset-password'),
-    url(r'^confirm-password/(?P<token>.+?)$',
-        ConfirmResetPassword.as_view(),
-        name="password_reset_done"),
-    url(r'^reset-password-done/',
-       ResetPasswordDoneAPIView.as_view(), name='reset-password-done'),
-    path('social_auth/', SocialAuth.as_view(), name="social_auth")
+    path('reset-password/', ResetPasswordAPIView.as_view(),
+         name='reset-password'),
+    path('confirm-password/<str:token>', ConfirmResetPassword.as_view(),
+         name="password_reset_done"),
+    path('reset-password-done/', ResetPasswordDoneAPIView.as_view(),
+         name='reset-password-done'),
 ]
