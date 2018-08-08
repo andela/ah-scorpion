02903b23aa80e6c32875f5e919aa154bf980a867
Samuel Munyili <samuel.munyili@andela.com>
2018-08-08 11:40:46 +0300

    add custom error message for login if user not verified

--- authors/apps/authentication/tests/test_user_verification.py
+++ authors/apps/authentication/tests/test_user_verification.py
@@ -19,6 +19,7 @@ class AuthenticationTests(APITestCase):
         }
         # Set up the registration url.
         self.reg_url = reverse('authentication:reg')
+        self.login_url = reverse('authentication:login')
         self.activate_url = 'authentication:activate'
 
     def test_invalid_token_passed(self):
@@ -46,7 +47,7 @@ class AuthenticationTests(APITestCase):
         """
         self.client.post(self.reg_url, self.data, format='json')
         token = generate_token(self.data, 0.01)
-        time.sleep(2)
+        time.sleep(1)
 
         self.activate_url = reverse(self.activate_url,
                                     kwargs={'token': token
@@ -105,3 +106,18 @@ class AuthenticationTests(APITestCase):
                 in str(response._container))
         self.assertEqual(response.status_code,
                          status.HTTP_401_UNAUTHORIZED)
+
+    def test_user_not_verified(self):
+        """
+        Tests if login will be rejected in the user email address has not
+        been verified.
+        Error 400 should be returned
+        :return:
+        """
+        self.client.post(self.reg_url, self.data, format='json')
+
+        response = self.client.post(self.login_url, self.data, format='json')
+        assert ("Please verify your email address to activate account" in str(
+            response._container))
+        self.assertEqual(response.status_code,
+                         status.HTTP_400_BAD_REQUEST)
