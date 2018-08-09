f6a54b3ce68cadadb63c4be84ca3c0e277c7069d
Samuel Munyili <samuel.munyili@andela.com>
2018-08-09 14:38:45 +0300

    change endpoints to use post and delete, not PUT

--- authors/apps/articles/views.py
+++ authors/apps/articles/views.py
@@ -1,9 +1,11 @@
 from django.utils.text import slugify
 import uuid
 from rest_framework import generics, status
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, \
+    IsAuthenticated
 from rest_framework.response import Response
 
+from authors.apps.profiles.renderers import ProfileJSONRenderer
 from .models import Article
 from .serializers import ArticleSerializer
 
@@ -31,7 +33,6 @@ class ArticleDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ArticleSerializer
     permission_classes = (IsAuthenticatedOrReadOnly,)
     lookup_field = 'slug'
-    print("We are *****????")
 
     def get_serializer_context(self):
         context = super(ArticleDetail, self).get_serializer_context()
@@ -47,7 +48,7 @@ class ArticleDetail(generics.RetrieveUpdateDestroyAPIView):
         return context
 
 
-class FavoriteArticle(generics.UpdateAPIView):
+class FavoriteArticle(generics.ListCreateAPIView, generics.DestroyAPIView):
     """
     Add or removes a user and article to the list of users liking different
     articles.
@@ -55,11 +56,55 @@ class FavoriteArticle(generics.UpdateAPIView):
     favourite it
     Else: The use no longer favourites the article
     """
+
+    permission_classes = (IsAuthenticated,)
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly,)
 
-    def update(self, request, slug):
+    def post(self, request, slug):
+        """
+        Helps user favourite article
+        If already, return message saying that the user has already favourated
+        Else make the user favourite article
+        :param request:
+        :param slug:
+        :return: article
+        ":return: response
+        """
+        try:
+            article = Article.objects.get(slug=slug)
+        except Article.DoesNotExist:
+            response = {"message": "The article was not found", }
+            return Response(response,
+                            status=status.HTTP_404_NOT_FOUND)
+        context = super(FavoriteArticle, self).get_serializer_context()
+        user = request.user
+
+        if user in article.favorited.all():
+            # Returns a message that the user has already favourited article
+
+            response = {
+                "message": "You have already marked "
+                           "this article as a favourite"}
+            return Response(response, status=status.HTTP_200_OK)
+
+        else:
+            # Add user from the list of users liking the particular article
+            article.favorited.add(user.id)
+
+            response = {"article": context['request'].data}
+            return Response(response, status=status.HTTP_200_OK)
+
+    def delete(self, request, slug):
+        """
+        Helps user un-favourite article
+        If already, return article
+        Else message saying that the user has not favourated article
+        :param request:
+        :param slug:
+        :return: article
+        ":return: response
+        """
 
         try:
             article = Article.objects.get(slug=slug)
@@ -67,20 +112,19 @@ class FavoriteArticle(generics.UpdateAPIView):
             response = {"message": "The article was not found", }
             return Response(response,
                             status=status.HTTP_404_NOT_FOUND)
+        context = super(FavoriteArticle, self).get_serializer_context()
 
         user = request.user
-        print("Article = ", article)
 
         if user in article.favorited.all():
             # Remove user from the list of users liking the particular article
             article.favorited.remove(user.id)
 
-            response = {"message": "This article is no longer your favourite"}
+            response = {"message": context['request'].data}
             return Response(response, status=status.HTTP_200_OK)
         else:
-            # Add user from the list of users liking the particular article
-            article.favorited.add(user.id)
-
-            response = {"message": "You have marked this article as your "
-                                   "favourite"}
+            # Returns a message that the user has already favourited article
+            response = {
+                "message": "You have not already marked "
+                           "this article as a favourite"}
             return Response(response, status=status.HTTP_200_OK)
