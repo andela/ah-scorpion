b29d90b76381209a5154d9fdf323756e6642c48d
Samuel Munyili <samuel.munyili@andela.com>
2018-08-09 11:57:54 +0300

    ft

--- authors/apps/articles/urls.py
+++ authors/apps/articles/urls.py
@@ -9,9 +9,12 @@ urlpatterns = [
     path('', views.ArticleList.as_view(), name='all_articles'),
     path('<str:slug>/', views.ArticleDetail.as_view(), name='article_detail'),
     path('<str:slug>/like/', views.LikeArticle.as_view(), name='like_article'),
-    path('<str:slug>/dislike/', views.DislikeArticle.as_view(), name='dislike_article'),
+    path('<str:slug>/dislike/', views.DislikeArticle.as_view(),
+         name='dislike_article'),
     path('<str:slug>/ratings', include('authors.apps.ratings.urls',
-                                       namespace='ratings'))
+                                       namespace='ratings')),
+    path('<str:slug>/favorite/', views.FavoriteArticle.as_view(),
+         name='article_favorite'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
