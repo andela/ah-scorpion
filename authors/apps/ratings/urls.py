6989165f1e80832e8f5ae84c201b96a9a630e0be
davenjeru <dave.njeru@andela.com>
2018-08-09 16:29:33 +0300

    [feature #159053987] add functionality to view an article's ratings and rate an article
    Signed-off-by: davenjeru <dave.njeru@andela.com>

--- authors/apps/ratings/urls.py
+++ authors/apps/ratings/urls.py
@@ -1,1 +1,9 @@
-# write your URLs here
+from django.urls import path
+
+from . import views
+
+app_name = "articles"
+
+urlpatterns = [
+    path('', views.RatingsRetrieveAPIView.as_view(), name='all_ratings'),
+]
