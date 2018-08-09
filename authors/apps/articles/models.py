72dcfc6e8218f7321e5e12d8d97cead1151b10d9
lenny <lennykmutua@gmail.com>
2018-08-09 15:08:08 +0300

    [feature #159053989] enable liking and disliking articles (#22)

--- authors/apps/articles/models.py
+++ authors/apps/articles/models.py
@@ -20,6 +20,9 @@ class Article(models.Model):
 
     author = models.ForeignKey(User, on_delete=models.CASCADE, db_column='author')
 
+    likes = models.ManyToManyField(User, related_name='likes', blank=True)
+    dislikes = models.ManyToManyField(User, related_name='dislikes', blank=True)
+
     def __str__(self):
         return self.title
 
--- authors/apps/articles/urls.py
+++ authors/apps/articles/urls.py
@@ -8,6 +8,8 @@ app_name = "articles"
 urlpatterns = [
     path('', views.ArticleList.as_view(), name='all_articles'),
     path('<str:slug>/', views.ArticleDetail.as_view(), name='article_detail'),
+    path('<str:slug>/like/', views.LikeArticle.as_view(), name='like_article'),
+    path('<str:slug>/dislike/', views.DislikeArticle.as_view(), name='dislike_article'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
