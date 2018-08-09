dd48644a984fd0099284370e90e91d75691fb682
Samuel Munyili <samuel.munyili@andela.com>
2018-08-09 11:58:28 +0300

    ft

--- authors/apps/articles/views.py
+++ authors/apps/articles/views.py
@@ -1,8 +1,9 @@
-from django.utils.text import slugify
 import uuid
+
+from django.utils.text import slugify
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
 
@@ -30,6 +31,7 @@ class ArticleDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ArticleSerializer
     permission_classes = (IsAuthenticatedOrReadOnly,)
     lookup_field = 'slug'
+    print("We are *****????")
 
     def get_serializer_context(self):
         context = super(ArticleDetail, self).get_serializer_context()
@@ -65,7 +67,8 @@ class LikeArticle(generics.UpdateAPIView):
         user = request.user
 
         # removes the user from the list of disliking users,
-        # nothing changes if the user does not exist in the list of disliking users
+        # nothing changes if the user does not exist in the list of
+        # disliking users
         article.dislikes.remove(user.id)
 
         # allows for the None option: you neither like nor dislike the article
@@ -115,3 +118,42 @@ class DislikeArticle(generics.UpdateAPIView):
 
         response = {"Message": "You have successfully disliked this article"}
         return Response(response, status=status.HTTP_200_OK)
+
+
+class FavoriteArticle(generics.UpdateAPIView):
+    """
+    Add or removes a user and article to the list of users liking different
+    articles.
+    If the user has not already favourited an article, he is updated to
+    favourite it
+    Else: The use no longer favourites the article
+    """
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def update(self, request, slug):
+
+        try:
+            article = Article.objects.get(slug=slug)
+        except Article.DoesNotExist:
+            response = {"message": "The article was not found", }
+            return Response(response,
+                            status=status.HTTP_404_NOT_FOUND)
+
+        user = request.user
+        print("Article = ", article)
+
+        if user in article.favorited.all():
+            # Remove user from the list of users liking the particular article
+            article.favorited.remove(user.id)
+
+            response = {"message": "This article is no longer your favourite"}
+            return Response(response, status=status.HTTP_200_OK)
+        else:
+            # Add user from the list of users liking the particular article
+            article.favorited.add(user.id)
+
+            response = {"message": "You have marked this article as your "
+                                   "favourite"}
+            return Response(response, status=status.HTTP_200_OK)
