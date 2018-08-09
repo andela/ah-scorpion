51e20c9b2d1fbc89349c3eb270b60281bc820235
Samuel Munyili <samuel.munyili@andela.com>
2018-08-09 11:57:54 +0300

    ft

--- authors/apps/articles/urls.py
+++ authors/apps/articles/urls.py
@@ -8,6 +8,8 @@ app_name = "articles"
 urlpatterns = [
     path('', views.ArticleList.as_view(), name='all_articles'),
     path('<str:slug>/', views.ArticleDetail.as_view(), name='article_detail'),
+    path('<str:slug>/favorite/', views.FavoriteArticle.as_view(),
+         name='article_favorite'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
