ef0ddabcca2f65d847de0db7c3656d6218572f2b
davenjeru <dave.njeru@andela.com>
2018-08-13 11:18:45 +0300

    change the 'app_name' from 'articles' to 'ratings'
    Signed-off-by: davenjeru <dave.njeru@andela.com>

--- authors/apps/ratings/urls.py
+++ authors/apps/ratings/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-app_name = "articles"
+app_name = "ratings"
 
 urlpatterns = [
     path('', views.RatingsRetrieveAPIView.as_view(), name='all_ratings'),
