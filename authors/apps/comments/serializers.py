d92195a29cbf2857cf95b271ea289fbef955d931
lenny <lennykmutua@gmail.com>
2018-08-15 17:51:57 +0300

    #159726516 Enable liking/disliking comments (#33)
    - Update README
    - Add like and dislike count fields

--- authors/apps/comments/serializers.py
+++ authors/apps/comments/serializers.py
@@ -7,14 +7,22 @@ from ..articles.serializers import ArticleSerializer
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    """serialize data for handling comments."""
+
     user = UserSerializer(read_only=True)
     article = ArticleSerializer(read_only=True)
+    likes = serializers.SerializerMethodField(method_name='get_likes_count')
+    dislikes = serializers.SerializerMethodField(
+        method_name='get_dislikes_count')
 
     class Meta:
+        """specify the model and fields to be used by the serializer."""
+
         model = Comment
         fields = '__all__'
 
     def create(self, validated_data):
+        """Handle creating a new comment."""
         slug = self.context['request'].data.get('slug')
         try:
             article = Article.objects.get(slug=slug)
@@ -23,11 +31,16 @@ class CommentSerializer(serializers.ModelSerializer):
         try:
             user = self.context['request'].user
         except self.context['request'].user.DoesNotExist:
-            raise \
-                PermissionDenied('Please log in first to perform this action')
+            raise PermissionDenied(
+                'Please log in first to perform this action')
         comment = Comment.objects.create(
-            user=user,
-            article=article,
-            **validated_data
-        )
+            user=user, article=article, **validated_data)
         return comment
+
+    def get_likes_count(self, instance):
+        """Get the total number of likes for a particular comment."""
+        return instance.likes.count()
+
+    def get_dislikes_count(self, instance):
+        """Get the total number of dislikes for a particular comment."""
+        return instance.dislikes.count()
