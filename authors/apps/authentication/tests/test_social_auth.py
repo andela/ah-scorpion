3ba70471c17319e9f2960301fd8bd5bf86315afa
lenny <lennykmutua@gmail.com>
2018-09-05 09:55:34 +0300

    [ch 160277232] Add mock test

--- authors/apps/authentication/tests/test_social_auth.py
+++ authors/apps/authentication/tests/test_social_auth.py
@@ -1,6 +1,9 @@
 from django.urls import reverse
 from rest_framework.test import APITestCase, APIClient
 
+from unittest.mock import patch
+from ..models import User
+
 
 class SocialAuthenticationTests(APITestCase):
     def setUp(self):
@@ -11,6 +14,13 @@ class SocialAuthenticationTests(APITestCase):
         self.auth_url = reverse('authentication:social_auth')
 
         self.client = APIClient()
+
+        self.user = User(
+            username='lenny',
+            email='lennykmutua@gmail.com',
+            password='123qwerty',
+            is_active=True)
+        self.user.save()
 
     def test_missing_access_token(self):
         """
@@ -46,3 +56,14 @@ class SocialAuthenticationTests(APITestCase):
         data = {"access_token": access_token, "provider": "facebook"}
         response = self.client.post(self.auth_url, data=data)
         self.assertEqual(response.status_code, 400)
+
+    @patch('authors.apps.authentication.views.load_backend')
+    def test_valid_authentication(self, mock_backend):
+        """
+        Test a successful user authentication using social_auth
+        """
+        mock_backend().do_auth.return_value = self.user
+        access_token = "AAexjwrTz4IBAC2T3cPCtLdLS3fUGVEz9Ma37"
+        data = {"access_token": access_token, "provider": "facebook"}
+        response = self.client.post(self.auth_url, data=data)
+        self.assertEqual(response.status_code, 200)
